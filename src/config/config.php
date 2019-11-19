c038f2e1079fe715e118e9c22f6e781eb233006d
Lakshya Gupta <46844653+lakshya-gupta12@users.noreply.github.com>
2019-11-19 21:22:31 +0530

    Add files via upload

---
+++ src/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+	define('ROOT_URL', 'http://localhost/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'root-password');
+	define('DB_NAME', 'your-database-name');
+
+
+ ?>
